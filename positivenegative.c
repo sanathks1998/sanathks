ffdd7da525e2fd928679b98f25df2901afa84dbc
sanathks1998 <sanathks1998@gmail.com>
2017-06-28 14:21:05 +0530

    Update positivenegative.c

--- positivenegative.c
+++ positivenegative.c
@@ -1,1 +1,14 @@
 #include<stdio.h>
+#include<conio.h>
+void main ()
+{
+  int n;
+  clrscr();
+  printf("entre a value");
+  scanf("%d",&n);
+  if(n>1)
+    printf("%d",n);
+  else
+    printf("0");
+  getch();
+}
