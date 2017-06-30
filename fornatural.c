da4605404a75739113a556ada4e3290174e8da3e
sanathks1998 <sanathks1998@gmail.com>
2017-06-30 10:49:23 +0530

    Update and rename naturalsum.c to fornatural.c

--- fornatural.c
+++ fornatural.c
@@ -2,14 +2,14 @@
 #include<conio.h>
 void main()
 {
-int a,sum=0,i=0;
+int a,sum=0,i;
 clrscr();
 printf("entre the limit");
 scanf("%d",&a);
-while(i<=a)
+for(i=0;i<=a;i++)
 {
   sum+=i;
-  i++;
+ 
 printf("\n%d",sum);
 
 getch();
