28b042d2ce66b512c3e481a28b2b2763e15a3d18
sanathks1998 <sanathks1998@gmail.com>
2017-07-10 12:26:48 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter the numbers");
+scanf("%d%d",&a,&b);
+    c=pow(a,b)    ;
+
+printf("\n %d ^%d=%d",a,b,c);
+
+
+getch();
+}
