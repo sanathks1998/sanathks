d841f777671bb20babc78b7b6eb7009c28c5c09a
sanathks1998 <sanathks1998@gmail.com>
2017-09-15 11:46:04 +0530

    Create evenprints.c

---
+++ evenprints.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m;
+printf("entre first limit and second limit");
+scanf("%d%d",&n,&m)
+do{
+  if(n%2==0)
+printf("\n%d",n);
+  n++;
+} while(n<m);
+getch();
+}
