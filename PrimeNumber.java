f61a61b848d6b46a55b1699358b670c12043fe14
sanathks1998 <sanathks1998@gmail.com>
2018-08-06 14:53:21 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,45 +1,56 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class PrimeNumber
 {
   public static void main(String args[])
   {
-    int f,l,i,j,c;
+    int f,l,i,j,c=1;
  
     Scanner sc=new Scanner(System.in);
     System.out.println("entre first interval");
     f=sc.nextInt();
     System.out.println("entre last interval");
     l=sc.nextInt();
-    for(j=f;j<=l;j++)
+    while(f<=l)
     {
-        if(f==2)
+   if(f==1)
+   {
+       c=0;
+       
+   }
+       if(f==2)
+       {
+           c=1;
+       }
+   
+   
+      for(i=2;i<=f/2;i++)
+      {
+        if(f%i==0)
         {
-            System.out.println(f);
-        }
-        for(i=2;i<=f/2;i++)
-        {
-            if(f%i==0)
-            {
-                c=0;
-                break;
-            }    
           
+          c=0;
+         break;
         
-        else 
-        {
-            c=1;
+    
+          
         }
+        else 
+        {c=1;
         
-        if (c==1)
+        }
+      }
+        if(c==1)
         {
          System.out.println(f);
-        } 
-        
-    }
-    }
+        }
+       
+    f++;    
+           
+      
+   
   
-  
+  }
  }     
-}  
+}
