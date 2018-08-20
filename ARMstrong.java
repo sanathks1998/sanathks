39bbd5bdc2900b737982ee63147d18e783d394db
sanathks1998 <sanathks1998@gmail.com>
2018-08-20 13:43:12 +0530

    Create ARMstrong.java

---
+++ ARMstrong.java
@@ -0,0 +1,22 @@
+import java.io.*
+import java.util.*;
+class ARMstrong
+{
+  public static void main(String args[])
+  {
+      Scanner sc=new Scanner(System.in;
+      int num=sc.nextInt();
+      int i;
+      i=n;
+      while(i%10!=0)
+      {
+        r=i%10;
+        sum=sum+(i*i*i);
+        i=i/10;
+      }
+      if(sum==num)
+      System.out.println("it is armstrong number");
+      
+     else
+      System.out.println("it is not armstrong number")
+        }}
