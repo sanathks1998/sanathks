8505df8051c856872d82aca8e7fa22ee6a25c654
sanathks1998 <sanathks1998@gmail.com>
2018-09-03 13:37:30 +0530

    Update FactNum.java

--- FactNum.java
+++ FactNum.java
@@ -1,17 +1,18 @@
 import java.io.*;
-import java.util.*l
+import java.util.*;
 public class FactNum
 {
   public static void main(String args[])
   {
     Scanner sc=new Scanner(System.in);
     System.out.println("entre a number");
-    int a=sc.nexInt();
-    int fact=0;
+int a=sc.nextInt();
+
+    int fact=1,i;
     for(i=1;i<=a;i++)
     {
-      fact*=i;
+      fact=fact*i;
     } 
     System.out.println(fact);
    }
- }  
+ } 
