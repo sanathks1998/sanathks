13264fd7f45d9bce82054e62165d97ca61b8a664
sanathks1998 <sanathks1998@gmail.com>
2018-09-03 12:31:48 +0530

    Update ARMstrong.java

--- ARMstrong.java
+++ ARMstrong.java
@@ -10,21 +10,21 @@ public class ARMstronglimit
       System.out.println("entre  a second limit");
       int lst=sc.nextInt();
       int i;
-      int r,sum=0,w=0;
+      int r,sum1,w=0;
     for(i=num;i<lst;i++)
     {
-         sum=0;
+         sum1=0;
         w=i;
         while(w>0)
         {
             r=w%10;
-            sum=sum+r*r*r;
+            sum1=sum1+r*r*r;
             w=w/10;
         }
     
       
     
-        if(sum==i)
+        if(sum1==i)
         System.out.println(i);
         
        
