c611d668e1597f861cd98b85b9f880d8ed3fc74e
sanathks1998 <sanathks1998@gmail.com>
2018-09-03 13:42:12 +0530

    Create NumMul.java

---
+++ NumMul.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class NumMul
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("entre a number");
+    int num=sc.nextInt();
+    for(int i=1;i<=5;i++)
+    {
+      System.out.print(i*num+" ");
+    } 
+   } 
+  } 
