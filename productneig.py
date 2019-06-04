1ca18595ba2866db223bcdb600fabaab4b39f5f9
sanathks1998 <sanathks1998@gmail.com>
2019-06-04 15:46:54 +0530

    Create productneig.py

---
+++ productneig.py
@@ -0,0 +1,20 @@
+r=int(input())
+l=list(map(int,input().split()))
+
+f=1
+i=1
+while(i<len(l)):
+  f=1
+  j=1
+  for x in l:
+  
+       if(i!=j):
+            
+            f=f*x
+            j=j+1
+       else:
+            j=j+1
+            continue
+  
+  print(f,end=" ")
+  i=i+1
