eeff265177fac2cf1bfb24e292030d6c8b1a47b5
sanathks1998 <sanathks1998@gmail.com>
2019-06-06 15:18:20 +0530

    Create sumpalli.py

---
+++ sumpalli.py
@@ -0,0 +1,21 @@
+s=0
+q=0
+r=int(input())
+while(r!=0):
+    t=r%10
+    s=s+t
+    r=r//10
+a=s
+print(a)
+if(s<9):
+    print("YES")
+else:
+    while(s!=0):
+        f=s%10
+        q=f+(q*10)
+        
+        s=s//10
+    if(a==q):
+        print("YES")
+    else:
+        print("NO")
